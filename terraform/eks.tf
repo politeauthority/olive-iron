77144b1a35fbf0f0da8d6a20633cef4714d39a8c
Alix Fullerton <fullertonalix0@gmail.com>
2023-08-02 21:07:09 -0400

    Adding EKS cluster creation and worker nodes

---
+++ terraform/eks.tf
@@ -0,0 +1,111 @@
+# Eks
+# Create the EKS cluster and all its supporting resources.
+#
+resource "aws_iam_role" "eks-iam-role" {
+    name = "eks-iam-role"
+    path = "/"
+    assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role    = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role    = aws_iam_role.eks-iam-role.name
+}
+
+resource "aws_eks_cluster" "cluster_one" {
+    name     = "${var.project}"
+    role_arn = aws_iam_role.eks-iam-role.arn
+    version  = "1.27"
+
+    vpc_config {
+        subnet_ids = [
+            aws_subnet.public_subnet[0].id,
+            aws_subnet.public_subnet[1].id,
+            aws_subnet.public_subnet[2].id,
+            aws_subnet.private_subnet[0].id,
+            aws_subnet.private_subnet[1].id,
+            aws_subnet.private_subnet[2].id
+        ]
+    }
+    depends_on = [
+        aws_iam_role.eks-iam-role,
+        aws_subnet.public_subnet,
+        aws_subnet.private_subnet
+    ]
+}
+
+resource "aws_iam_role" "workernodes" {
+    name = "${var.project}-eks-node-group"
+ 
+    assume_role_policy = jsonencode({
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            }
+        }]
+        Version = "2012-10-17"
+    })
+    depends_on = [
+        aws_eks_cluster.cluster_one,
+    ]
+}
+ 
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role       = aws_iam_role.workernodes.name
+}
+ 
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    role       = aws_iam_role.workernodes.name
+}
+ 
+resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+    policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+    role       = aws_iam_role.workernodes.name
+}
+ 
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_eks_node_group" "worker-node-group" {
+    cluster_name    = "${var.project}"
+    node_group_name = "${var.project}-workernodes"
+    node_role_arn   = aws_iam_role.workernodes.arn
+    subnet_ids      = [aws_subnet.private_subnet[0].id, aws_subnet.public_subnet[0].id]
+    instance_types  = ["t3.xlarge"]
+ 
+    scaling_config {
+        desired_size = 1
+        max_size     = 1
+        min_size     = 1
+    }
+ 
+  depends_on = [
+        aws_eks_cluster.cluster_one,
+        aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+        aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+        aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
--- terraform/olive-iron.tfvars
+++ terraform/olive-iron.tfvars
@@ -3,5 +3,5 @@ environment           = "production"
 region                = "us-east-2"
 availability_zones    = ["us-east-2a", "us-east-2b", "us-east-2c"]
 vpc_cidr              = "10.0.0.0/16"
-public_subnets_cidr    = ["10.0.1.0/24"]
-private_subnets_cidr   = ["10.0.255.0/24"]
+public_subnets_cidr    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+private_subnets_cidr   = ["10.0.253.0/24", "10.0.254.0/24", "10.0.255.0/24"]
